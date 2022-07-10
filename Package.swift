eb7e10248bcdb808f568edcfa16a50cef2968f60
fredz999 <jjjj58@hotmail.com>
2022-07-10 16:44:44 +0100

    initial

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "CursorGrid_Central_State_Package",
+    platforms: [
+            .iOS(.v15), .macOS(.v10_15)
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
