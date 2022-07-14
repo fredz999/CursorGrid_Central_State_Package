21663452319f39635eb46dc62f87bd74e81db4ff
fredz999 <jjjj58@hotmail.com>
2022-07-14 18:57:43 +0100

    _

--- Sources/CursorGrid_Central_State_Package/CursorGrid_Central_State_Object.swift
+++ Sources/CursorGrid_Central_State_Package/CursorGrid_Central_State_Object.swift
@@ -134,8 +134,12 @@ public class CursorGrid_Central_State {
                 lclTargetFunc4(intParam)
             }
             
-            if let lclQuadRef = cursor_X_Func_3 {
-                lclQuadRef(shared_Cursor_X_Int,shared_Cursor_Y_Int,shared_Cursor_DataX_Int,shared_Cursor_DataY_Int )
+            if noteWritingActive == true {
+                print("noteWritingActive == true got triggered")
+                if let lclQuadRef = cursor_X_Func_3 {
+                    print("noteWritingActive == true got triggered with cursor_X_Func_3")
+                    lclQuadRef(shared_Cursor_X_Int,shared_Cursor_Y_Int,shared_Cursor_DataX_Int,shared_Cursor_DataY_Int )
+                }
             }
             
         }
